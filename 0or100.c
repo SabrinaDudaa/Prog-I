29707fc80de9c5c46d92a6f96c6e1216e74b71ca
Sabrina <114834595+SabrinaDudaa@users.noreply.github.com>
2022-10-06 08:35:26 -0300

    Number 6 - Prog I

---
+++ 0or100.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float n;
+    printf("Escreva um numero",n);
+    scanf("%f",&n);
+
+    if (n > 100)
+    printf("Seu numero: ",n);
+
+    else
+    printf("0");
+
+    return 0;
+}
